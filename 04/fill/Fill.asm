f12957061475e722669116bec84dd60843c9a6bd
Richard Guan <rguan72@gmail.com>
2022-03-10 11:55:58 -0500

    Implement making whole screen black

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -10,5 +10,36 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
 // Put your code here.
+// 256 rows, 512 columns
+// 256 rows, 512/16=32 words per row. 256*32=8192 words in screen (8192 * 16 pixels)
+
+    @i
+    M=0 // i=0
+
+    // go to end if i >= 8192
+(LOOP)
+    @i
+    D=M
+    @8192
+    D=D-A
+    @END
+    D;JGE 
+
+    // M[screen + i] = -1
+    @i
+    D=M
+    @SCREEN
+    A=D+A
+    M=-1
+
+    // i++
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
