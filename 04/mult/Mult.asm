5d00eaa2e30dcffe1f8fa01e42038bec6e58e9c2
Richard Guan <rguan72@gmail.com>
2022-03-10 14:30:17 -0500

    Implement naive multiplication algorithm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,45 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// prod = 0
+    @prod
+    M=0
+
+// i = R1
+    @R1
+    D=M
+    @i
+    M=D
+
+// if i == 0 go to ENDLOOP
+(LOOP)
+    @i
+    D=M
+    @ENDLOOP
+    D;JEQ
+
+// prod += R0
+    @R0
+    D=M
+    @prod
+    M=D+M
+
+// i -= 1
+    @i
+    M=M-1
+
+// jump to LOOP
+    @LOOP
+    0;JMP
+
+(ENDLOOP)
+    // R2 = prod
+    @prod
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0;JMP
